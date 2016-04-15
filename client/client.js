8ac7ab964309bfa6d71407ad5bcd756d411296cf
wallacewylie <wallace.wylie@gmail.com>
2016-04-15 14:43:18 -0500

    Controllers

--- client/client.js
+++ client/client.js
@@ -1,3 +1,40 @@
-/**
- * Created by wallacewylie on 3/24/16.
- */
+var app = angular.module('abbeyApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainController'
+        })
+        .when('/craft', {
+            templateUrl: 'views/craft.html',
+            controller: 'CraftController'
+        })
+        .when('/scotland', {
+            templateUrl: 'views/scotland.html',
+            controller: 'ScotlandController'
+        })
+        .when('/wedding', {
+            templateUrl: 'views/wedding.html',
+            controller: 'WeddingController'
+        });
+
+    $locationProvider.html5Mode(true);
+}]);
+
+
+app.controller('MainController', ['$scope', function($scope){
+
+}]);
+
+app.controller('CraftController', ['$scope', function($scope){
+
+}]);
+
+app.controller('ScotlandController', ['$scope', function($scope){
+
+}]);
+
+app.controller('WeddingController', ['$scope', function($scope){
+
+}]);
